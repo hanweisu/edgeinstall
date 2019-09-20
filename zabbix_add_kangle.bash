47f834f8cbf9e2783112327a71bc363ce3815b71
root <root@HK2-Edge7C80.(none)>
2019-09-20 15:59:43 +0800

    Update zabbix kangle sessions for dollar sign

--- zabbix_add_kangle.bash
+++ zabbix_add_kangle.bash
@@ -4,8 +4,8 @@ test ! -d /etc/zabbix/scripts && echo "Zabbix scripts directory doesn't exist, c
 tee -a /etc/zabbix/scripts/kangle_status.sh << END
 #/bin/sh
 curl http://127.0.0.1:3311/core.whm?whm_call=info &>/dev/null  > /tmp/kangle-info.xml
-echo cat // $1 | xmllint --shell /tmp/kangle-info.xml | sed '/^\/ >/d' | sed 's/<[^>]*.//g'
-rm -f /tmp/kangle-info.xml
+echo cat // \$1 | xmllint --shell /tmp/kangle-info.xml | sed '/^\/ >/d' | sed 's/<[^>]*.//g'
+echo rm -f /tmp/kangle-info.xml
 END
 chmod +x /etc/zabbix/scripts/kangle_status.sh
 
